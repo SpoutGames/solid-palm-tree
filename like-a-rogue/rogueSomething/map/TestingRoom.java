9d7b0e3377188b7dde6404e463898a680a50a6c6
Ackeso <zedhead1>
2018-03-17 21:24:29 -0500

    testingroomstuff

--- like-a-rogue/rogueSomething/map/TestingRoom.java
+++ like-a-rogue/rogueSomething/map/TestingRoom.java
@@ -5,13 +5,13 @@ import java.awt.Graphics;
 import player.Player;
 import state.Main;
 import state.State;
-
-public class TestingRoom extends State{
+import map.World;
+public class TestingRoom extends World{
 	
 	private Player player;
 	
 	public TestingRoom(Main game) {
-		super(game);
+		super.super(game);
 		player = new Player(game, 0, 0);
 	}
 
