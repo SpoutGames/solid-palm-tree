26b5d3fd90f80a9bb59749c7eb89cfaa4cc577c3
Ispoutgames <35543189+Ispoutgames@users.noreply.github.com>
2018-03-26 16:02:09 -0500

    menu changes

--- like-a-rogue/rogueSomething/state/MainMenu.java
+++ like-a-rogue/rogueSomething/state/MainMenu.java
@@ -15,7 +15,7 @@ public class MainMenu extends State{
 	Main game = new Main();
 	
 	//privates
-		private boolean rowToggleLock = false;
+		private boolean rowToggleLock = false, displayDevelopment = false;
 		private int row = 0, selection = 0, cursorFix = 0;
 	
 	//publics
@@ -33,37 +33,73 @@ public class MainMenu extends State{
 	
 	//functions
 		public void menuSelection() {
-			if (row == 0) { //singlePlayer
-				cursorFix = 50;
-			} else if (row == 1) { //multiplayer
-				cursorFix = 50;
-			} else if (row == 2) { //options
-				cursorFix = 30;
-			} else if (row == 3) { //exit
-				cursorFix = 10;
-				if (handler.getInput().enter) {
-					System.exit(0);
+			
+			//main screen selections
+				if (selection == 0) {
+						if (row == 0) {			//singlePlayer
+							
+							cursorFix = 50;
+							
+							//selection
+								if (handler.getInput().enter) {
+									
+								}
+							
+						} else if (row == 1) {	//multiplayer
+							
+							cursorFix = 50;
+							
+							//selection
+								if (handler.getInput().enter) {
+									displayDevelopment = true;
+								}
+							
+						} else if (row == 2) {	//options
+							
+							cursorFix = 30;
+							
+							//selection
+								if (handler.getInput().enter) {
+									
+								}
+							
+						} else if (row == 3) { 	//exit
+							
+							cursorFix = 10;
+							//selection
+								if (handler.getInput().enter) {
+									System.exit(0);
+								}
+						}
 				}
-			}
-			if (handler.getInput().up || handler.getInput().left) {
-				if (!rowToggleLock) {
-					if (row - 1 >= 0) {
-						row -= 1;
+			
+			
+			//cursor movement
+				if (handler.getInput().up || handler.getInput().left) {
+					if (!rowToggleLock) {
+						if (row - 1 >= 0) {
+							row -= 1;
+							displayDevelopment = false;
+						}
 					}
+					rowToggleLock = true;
 				}
-				rowToggleLock = true;
-			}
-			if (handler.getInput().down || handler.getInput().right) {
-				if (!rowToggleLock) {
-					if (row + 1 <= 3) {
-						row += 1;
+				
+				if (handler.getInput().down || handler.getInput().right) {
+					if (!rowToggleLock) {
+						if (row + 1 <= 3) {
+							row += 1;
+							displayDevelopment = false;
+						}
 					}
+					rowToggleLock = true;
 				}
-				rowToggleLock = true;
-			}
-			if (!handler.getInput().down && !handler.getInput().up && !handler.getInput().left && !handler.getInput().right) {
-				rowToggleLock = false;
-			}
+				
+				
+			//lock reset
+				if (!handler.getInput().down && !handler.getInput().up && !handler.getInput().left && !handler.getInput().right) {
+					rowToggleLock = false;
+				}
 		}
 		public void menuText() {
 			
@@ -83,16 +119,33 @@ public class MainMenu extends State{
 			g.setColor(Color.BLACK);
 			g.fillRect(0, 0, (int)handler.getWidth(), (int)handler.getHeight());
 			
-			if (selection == 0) {
-				g.setColor(Color.WHITE);
-				g.setFont(new Font("serif", Font.PLAIN, 20));
-				g.drawImage(Assets.mainMenuCursor, 0+cursorFix+150*row, (int) (handler.getHeight()-50), null);
-				g.drawString("SinglePlayer", (int)(0+10), (int)handler.getHeight()-20);
-				g.drawString("Multiplayer", (int)(0+10+150), (int)handler.getHeight()-20);
-				g.drawString("Options", (int)(0+10+150*2), (int)handler.getHeight()-20);
-				g.drawString("Exit", (int)(0+10+150*3), (int)handler.getHeight()-20);
-			}
+			g.setColor(Color.WHITE);
+			g.setFont(new Font("serif", Font.BOLD, 40));
+			g.drawString("BlackROGUE", (int) (handler.getWidth()/2-150), 50);
 			
+			
+			//selections
+				if (selection == 0) {	//Singleplayer
+					g.setColor(Color.WHITE);
+					g.setFont(new Font("serif", Font.PLAIN, 20));
+					g.drawImage(Assets.mainMenuCursor, 0+cursorFix+150*row, (int) (handler.getHeight()-50), null);
+					g.drawString("SinglePlayer", (int)(0+10), (int)handler.getHeight()-20);
+					g.drawString("Multiplayer", (int)(0+10+150), (int)handler.getHeight()-20);
+					g.drawString("Options", (int)(0+10+150*2), (int)handler.getHeight()-20);
+					g.drawString("Exit", (int)(0+10+150*3), (int)handler.getHeight()-20);
+				}
+				if (selection == 1) {	//Multiplier
+					
+				}
+				if (selection == 2) {	//Options
+					
+				}
+			
+			//In development message
+				if (displayDevelopment) {
+					g.setFont(new Font("serif", Font.PLAIN, 15));
+					g.drawString("In Development", 0+10+150, (int) (handler.getHeight()-60));
+				}
 		}
 
 }
